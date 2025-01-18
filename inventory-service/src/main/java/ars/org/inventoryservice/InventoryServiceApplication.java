a21520f877e1ad97b0ed9c645783d7f4ceaa8eb0
ars <abderaouf.souissi@outlook.com>
2025-01-18 21:52:55 +0100

    added dynamic routing to gateway service in the application.ym file but the lower case config is preventing access to the services

--- inventory-service/src/main/java/ars/org/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/ars/org/inventoryservice/InventoryServiceApplication.java
@@ -2,15 +2,14 @@ package ars.org.inventoryservice;
 
 import ars.org.inventoryservice.entity.Product;
 import ars.org.inventoryservice.repository.ProductRepository;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.context.annotation.Bean;
 
 import java.util.UUID;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
